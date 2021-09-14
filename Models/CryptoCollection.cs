be4db7029b47b0273e5e4f203a65ac5ece5c353f
natekolozsvari <nate.kolozsvari@gmail.com>
2021-09-14 07:56:24 +0200

    Create CryptoCollection model

---
+++ Models/CryptoCollection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CryppitBackend.Models
+{
+    public class CryptoCollection
+    {
+        public List<Crypto> CryptoList { get; set; }
+    }
+}
