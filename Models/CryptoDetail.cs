2e0aee0922c9727ece6e3729ef47ff9bcf013ab9
natekolozsvari <nate.kolozsvari@gmail.com>
2021-09-14 08:00:07 +0200

    Create CryptoDetail model

---
+++ Models/CryptoDetail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CryppitBackend.Models
+{
+    public class CryptoDetail
+    {
+        public string Id { get; set; }
+
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+
+        public double Value { get; set; }
+        public double MarketCap { get; set; }
+        public double High24 { get; set; }
+        public double Low24 { get; set; }
+
+        public double AllTimeHigh { get; set; }
+        public double TotalValue { get; set; }
+
+        public string Image { get; set; }
+
+        public double Change { get; set; }
+    }
+}
