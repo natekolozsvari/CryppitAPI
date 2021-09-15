41d6d8c4b752b2a32c57cede4c7a2844f78fab44
Taowathar <taowathar@gmail.com>
2021-09-15 13:34:44 +0200

    Add Email property to User class

--- Models/User.cs
+++ Models/User.cs
@@ -7,6 +7,7 @@ namespace CryppitBackend.Models
 {
     public class User
     {
+        public string Email { get; set; }
         public string Name { get; set; }
         public string  Password { get; set; }
         public string Id { get; set; }
