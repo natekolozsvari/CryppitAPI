23c778c9e8382749a6f987b96ba9ccfe242d05be
Taowathar <taowathar@gmail.com>
2021-09-15 09:39:15 +0200

    Implement CryptoDetailService class

---
+++ Services/CryptoDetailService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using CryppitBackend.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace CryppitBackend.Services
+{
+    public class CryptoDetailService
+    {
+        public async Task<CryptoDetail> GetDetails(string cryptoId)
+        {
+            string baseURL = $"https://api.coingecko.com/api/v3/coins/{cryptoId}";
+            CryptoDetail cryptoDetail = null;
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    using (HttpResponseMessage res = await client.GetAsync(baseURL))
+                    {
+                        using (HttpContent content = res.Content)
+                        {
+                            string data = await content.ReadAsStringAsync();
+                            if (data != null)
+                            {
+                                cryptoDetail = JsonSerializer.Deserialize<CryptoDetail>(data, new JsonSerializerOptions
+                                {
+                                    PropertyNameCaseInsensitive = true
+                                });
+
+                                Trace.WriteLine("jhhz");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Data is null!");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+
+            }
+            return cryptoDetail;
+        }
+
+        public CryptoDetailService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+    }
+}
