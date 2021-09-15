0c871ea76e9228335f2d8483419eb5333545ac10
Taowathar <taowathar@gmail.com>
2021-09-15 11:11:37 +0200

    Add UserService class

---
+++ Services/UserService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using CryppitBackend.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace CryppitBackend.Services
+{
+    public class UserService
+    {
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        public UserService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        private string JsonFileName => Path.Combine(WebHostEnvironment.ContentRootPath, "Data", "users.json");
+    }
+}
