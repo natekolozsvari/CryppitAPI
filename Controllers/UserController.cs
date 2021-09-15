9185f01bbc5f7383e168d158783ac699f284bf5f
Taowathar <taowathar@gmail.com>
2021-09-15 11:13:34 +0200

    Add UserController class

---
+++ Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using CryppitBackend.Models;
+using CryppitBackend.Services;
+
+namespace CryppitBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public UserService UserService { get; }
+
+        public UserController(UserService userService)
+        {
+            this.UserService = userService;
+        }
+    }
+}
