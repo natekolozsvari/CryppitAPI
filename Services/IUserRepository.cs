7646454d889f14cc2de770305bb37b8ad5e54f33
Taowathar <taowathar@gmail.com>
2021-10-18 11:35:27 +0200

    Add IUserRepository interface

---
+++ Services/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CryppitBackend.Models;
+
+namespace CryppitBackend.Services
+{
+    public interface IUserRepository
+    {
+        User GetUSer(int id);
+        IEnumerable<User> GetAllUsers();
+        User Add(User user);
+        User Update(User user);
+        User Delete(int id);
+    }
+}
