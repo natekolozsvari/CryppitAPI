c87ff3a0487c6601f56f294606417279da7bab3f
natekolozsvari <nate.kolozsvari@gmail.com>
2021-10-18 11:04:05 +0200

    Configure DBContext

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CryppitBackend.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Investment> Investments { get; set; }
+
+        public DbSet<Crypto> Favorites { get; set; }
+
+        public DbSet<User> Users { get; set; }
+ 
+        public DbSet<DailyCrypto> Daily { get; set; }
+
+    }
+}
