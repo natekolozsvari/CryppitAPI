b1a55f8b28478e1545f9c8660fb785fd9e0be392
Taowathar <taowathar@gmail.com>
2021-10-18 11:53:12 +0200

    Add SQLUserRepository.cs

---
+++ Services/SQLUserRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CryppitBackend.Models;
+
+namespace CryppitBackend.Services
+{
+    public class SQLUserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+
+        public SQLUserRepository(AppDbContext context)
+        {
+            this._context = context;
+        }
+
+        public User GetUSer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Add(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Update(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
