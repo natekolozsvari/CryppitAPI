d665b5426f1704ce767e032e4a6d2f0f39cd16fc
natekolozsvari <nate.kolozsvari@gmail.com>
2021-10-18 11:35:01 +0200

    Create interface for daily crypto repository

---
+++ Services/IDailyRepository.cs
@@ -0,0 +1,14 @@
+﻿using CryppitBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CryppitBackend.Services
+{
+    interface IDailyRepository
+    {
+        DailyCrypto GetDaily();
+        DailyCrypto ChangeDaily();
+    }
+}
