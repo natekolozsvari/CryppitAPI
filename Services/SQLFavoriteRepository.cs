afe60203cbd06c143ac84a6a49fba29083d81eb0
Taowathar <taowathar@gmail.com>
2021-10-18 13:02:04 +0200

    Add SQLFavoriteRepository.cs

---
+++ Services/SQLFavoriteRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CryppitBackend.Models;
+
+namespace CryppitBackend.Services
+{
+    public class SQLFavoriteRepository : IFavoriteRepository
+    {
+        readonly AppDbContext _context;
+
+        public SQLFavoriteRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Favorite> GetAllFavorites()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Favorite Add(Favorite favorite)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Favorite Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
