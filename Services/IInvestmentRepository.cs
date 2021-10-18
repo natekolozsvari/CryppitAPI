d1ef702882e36f75c76e40cd83902cfd80e2c0f3
natekolozsvari <nate.kolozsvari@gmail.com>
2021-10-18 11:14:42 +0200

    Create InvestmentRepository interface

---
+++ Services/IInvestmentRepository.cs
@@ -0,0 +1,18 @@
+﻿using CryppitBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CryppitBackend.Services
+{
+    interface IInvestmentRepository
+    {
+        Investment GetInvestment(int id);
+        IEnumerable<Investment> GetAllInvestments();
+        IEnumerable<Investment> GetInvestmentsForUser(int userId);
+        Investment Add(Investment investment);
+        Investment Update(Investment investmentChanges);
+        Investment Delete(int id);
+    }
+}
