5c24a07a4430ae2e59b9468261a0e33c60b73d0a
Taowathar <taowathar@gmail.com>
2021-10-21 12:02:11 +0200

    Add Salt column to user table

--- Migrations/20211021094327_Add salt column to user table.Designer.cs
+++ Migrations/20211021094327_Add salt column to user table.Designer.cs
@@ -4,14 +4,16 @@ using CryppitBackend.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CryppitBackend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211021094327_Add salt column to user table")]
+    partial class Addsaltcolumntousertable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
